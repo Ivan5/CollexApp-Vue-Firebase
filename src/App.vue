95906d43e1d590e8cc36795f5f0439481db479b5
Ivan5 <converse_ivan@hotmail.com>
2018-07-01 00:59:41 -0500

    Connecto to Database

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>Items in Firebase</h1>
+    <div v-for="item in items" :key="item.id">
+      <h3>{{ item.title }}</h3>
+      <h4>{{ item.author }}</h4>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+//import HelloWorld from './components/HelloWorld'
+import Firebase from 'firebase'
+let config = {
+  apiKey: "AIzaSyAWvqSSZBRtn3JQTUhJl9zxz9i_zc7uQMk",
+  authDomain: "collex-6a629.firebaseapp.com",
+  databaseURL: "https://collex-6a629.firebaseio.com",
+  projectId: "collex-6a629",
+  storageBucket: "collex-6a629.appspot.com",
+  messagingSenderId: "414898349696"
+}
+let app = Firebase.initializeApp(config);
+let db = app.database()
+let itemsRef = db.ref('items');
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  firebase:{
+    items: itemsRef
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App'
 
-Vue.config.productionTip = false
-
+//Vue.config.productionTip = false
+Vue.use(VueFire);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
